8eb942a52564f1db1b2490f2fe5e8c4188c69191
Sachal Mohar <sachal16@uvic.ca>
2025-12-21 22:03:35 -0800

    entry point reset

--- src/com/sachal/githubactivity/Main.java
+++ src/com/sachal/githubactivity/Main.java
@@ -3,5 +3,6 @@ package com.sachal.githubactivity;
 public class Main {
     public static void main(String[] args) {
         System.out.println("github-useractivity started");
+        System.out.println("github-useractivity tes");
     }
 }
