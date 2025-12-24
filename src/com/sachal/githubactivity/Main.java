d43b91d15c43987c2810685a392762b8dbb3c410
Sachal Mohar <sachal16@uvic.ca>
2025-12-23 18:29:53 -0800

    Username-noUsername use case

--- src/com/sachal/githubactivity/Main.java
+++ src/com/sachal/githubactivity/Main.java
@@ -2,7 +2,21 @@ package com.sachal.githubactivity;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("github-useractivity started");
-        System.out.println("github-useractivity tes");
+
+
+        //no usernamegiven -> printed Usage
+        if(args.length==0){
+            System.out.println("Usage: github-activity <username>");
+            return;
+        }else{
+            String username=args[0];
+            username=username.trim();
+            if(username.length()==0){
+                System.out.println("Invalid username");
+            }else{
+                System.out.println(username);
+            }
+        }
+
     }
 }
