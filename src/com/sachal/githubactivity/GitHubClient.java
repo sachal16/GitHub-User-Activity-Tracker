26b10a00d3ca7632e8a14702fbf78d22c4b29d51
Sachal Mohar <sachal16@uvic.ca>
2025-12-24 19:45:58 -0800

    Setting up the Clien, Request with Builder

--- src/com/sachal/githubactivity/GitHubClient.java
+++ src/com/sachal/githubactivity/GitHubClient.java
@@ -1,11 +1,28 @@
 package com.sachal.githubactivity;
 
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.URI;
+
 public class GitHubClient {
 
     public ApiResponse wiring(String username){
         ApiResponse api = new ApiResponse(0,"placeholder");
         String url = "https://api.github.com/users/" + username +"/events";
         System.out.println(url);
+
+        //first string = h-name
+        //second string = h-value
+        HttpClient client = HttpClient.newHttpClient();
+        URI uri = URI.create(url);
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(uri)
+                .GET()
+                .header("Accept", "application/vnd.github+json")
+                .header("User-Agent", "github-useractivity")
+                .build();
+
+
         return api;
     }
 
